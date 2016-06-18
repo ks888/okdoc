c3c5f712a2369c4fa103abf6660b14afe5ef1538
Kishin Yagami <k.yagami.suou@gmail.com>
2016-06-18 16:57:06 +0900

    Use runner package

--- testset/testset.go
+++ testset/testset.go
@@ -1,19 +1,18 @@
 package testset
 
 import (
-	"errors"
 	"fmt"
 	"io/ioutil"
-	"os/exec"
+	"path/filepath"
 
 	"github.com/ks888/okdoc/parser"
+	"github.com/ks888/okdoc/runner"
 )
 
 type Test struct {
 	content *parser.CodeBlock
 	name    string
-	result  string
-	output  string
+	result  *runner.RunResult
 }
 
 type TestFile struct {
@@ -25,8 +24,8 @@ type TestSet struct {
 	testFiles []*TestFile
 }
 
-func (ts *TestSet) AddTestFile(filepath string) error {
-	fileContent, err := ioutil.ReadFile(filepath)
+func (ts *TestSet) AddTestFile(path string) error {
+	fileContent, err := ioutil.ReadFile(path)
 	if err != nil {
 		return err
 	}
@@ -34,9 +33,12 @@ func (ts *TestSet) AddTestFile(filepath string) error {
 	md_content := &parser.Markdown{Content: string(fileContent)}
 	md_content.Parse()
 
-	testFile := &TestFile{path: filepath}
+	testFile := &TestFile{path: path}
+	filename := filepath.Base(path)
+
 	for i, codeBlock := range md_content.CodeBlocks {
-		test := &Test{content: codeBlock, name: string(i)}
+		testName := fmt.Sprintf("%s#%d", filename, i)
+		test := &Test{content: codeBlock, name: testName}
 		testFile.list = append(testFile.list, test)
 	}
 	ts.testFiles = append(ts.testFiles, testFile)
@@ -46,24 +48,13 @@ func (ts *TestSet) AddTestFile(filepath string) error {
 
 func (ts *TestSet) RunAllTests() error {
 	for _, testFile := range ts.testFiles {
-		fmt.Printf("TestFile: %s\n", testFile.path)
-
 		for _, test := range testFile.list {
-			testScriptFile, err := ioutil.TempFile("", "okdoc_")
-			if err != nil {
-				return errors.New("Failed to create temp file")
-			}
+			runnerInst := runner.FindRunner(test.content.Lang)
 
-			_, err = testScriptFile.WriteString(test.content.Block)
-			if err != nil {
-				return errors.New("Failed to write test script to temp file")
-			}
-
-			// 'out' includes stdout AND stderr
-			out, err := exec.Command(test.content.Lang, testScriptFile.Name()).CombinedOutput()
-			test.output = string(out)
-			if err != nil {
-				test.result = err.Error()
+			if runnerInst == nil {
+				test.result = &runner.RunResult{true, false, "No test runner"}
+			} else {
+				test.result = runnerInst.Run(test.content.Block)
 			}
 		}
 	}
@@ -72,21 +63,30 @@ func (ts *TestSet) RunAllTests() error {
 
 func (ts *TestSet) PrintTestStats() {
 	numTests := 0
+	numNoRunnerTests := 0
 	failedTests := make([]*Test, 0)
 	for _, testFile := range ts.testFiles {
 		for _, test := range testFile.list {
-			if test.result != "" {
+			if !test.result.HasRunner {
+				numNoRunnerTests++
+				continue
+			}
+
+			if !test.result.Success {
 				failedTests = append(failedTests, test)
 			}
 			numTests++
 		}
 	}
 
-	fmt.Printf("%d of %d tests are successful!\n", numTests-len(failedTests), numTests)
+	fmt.Printf("%d of %d tests are successful! (Plus, %d tests have no test runner)\n", numTests-len(failedTests), numTests, numNoRunnerTests)
 	if len(failedTests) != 0 {
-		fmt.Printf("The list of failed tests: \n")
+		fmt.Printf("\nThe list of failed tests: \n")
+
 		for _, test := range failedTests {
-			fmt.Printf("* %s: %s\n", test.name, test.result)
+			fmt.Printf("====== %s ======\n", test.name)
+			fmt.Printf("%s\n", test.result.Message)
+			fmt.Printf("===============\n")
 		}
 	}
 
